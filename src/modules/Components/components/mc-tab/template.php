5951ba8d9b3dc9319e48875f345342ecf3a37b4e
Lucas Pirola <lucaspirola@gmail.com>
2024-11-19 23:00:46 -0300

    feat(acessibilidade): VII: Associa nome a tab panel das abas

--- src/modules/Components/components/mc-tab/template.php
+++ src/modules/Components/components/mc-tab/template.php
@@ -4,6 +4,6 @@
  * @var MapasCulturais\Themes\BaseV2\Theme $this
  */
 ?>
-<section v-if="isActive || cached" v-show="isActive" :aria-hidden="!isActive" class="tab-component" :class="slug" role="tabpanel">
+<section v-if="isActive || cached" v-show="isActive" :aria-hidden="!isActive" :aria-labelledby="aba" class="tab-component" :class="slug" role="tabpanel">
     <slot></slot>
 </section>
