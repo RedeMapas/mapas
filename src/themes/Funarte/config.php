9cb5d294d8351d1ac9a5dd2f97efa3ca1b3b03e1
Davi Pontes <davi.luciano1@gmail.com>
2025-02-11 03:25:35 +0000

    Descrição do que foi feito

---
+++ src/themes/Funarte/config.php
@@ -0,0 +1,19 @@
+<?php 
+use \MapasCulturais\i;
+
+return [
+    /* Primeira linha do logo configurável */
+    'logo.title' => 'Rede',
+
+    /* Segunda linha do logo configurável */
+    'logo.subtitle' => 'das Artes',
+
+    /* 
+    Define o nome do asset da imagem da logo do site - Substituirá a logo padrão
+
+    ex: `img/meu-mapa-logo.jpg` (pasta assets/img/meu-mapa-logo.jpg do tema) 
+    */
+    'logo.image' => 'img/marca-rede-das-artes.svg',
+
+
+];
