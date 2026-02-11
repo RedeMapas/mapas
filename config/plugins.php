8d35a0afed3d7e7474e20253379f9cda3450fad6
Lucas Pirola <lucaspirola@gmail.com>
2026-02-10 22:31:47 -0300

    chore(config): update project configuration for multi-environment setup

--- config/plugins.php
+++ config/plugins.php
@@ -26,5 +26,19 @@ return [
                 },
             ]
         ],
+         'MapasNetwork' => [
+             'namespace' => 'MapasNetwork',
+             'config' => [
+                 'nodes' => [
+                     'https://mapa.cultura.gov.br/',
+                     'https://mapacultural.pa.gov.br/',
+                     'https://mapacultural.secult.ce.gov.br/',
+                 ],
+                 'filters' => [
+                     'agent' => ['En_Estado' => 'BR'],
+                     'space' => ['En_Estado' => 'BR'],
+                 ],
+             ],
+         ],
     ]
 ];
