e5ed504a7cb23a839c20f25a742f8132e957f8c0
Lucas Pirola <lucaspirola@gmail.com>
2026-02-23 15:17:25 -0300

    feat: upgrade php 8.4

--- src/modules/OpportunityPhases/Module.php
+++ src/modules/OpportunityPhases/Module.php
@@ -20,7 +20,7 @@ class Module extends \MapasCulturais\Module{
      *
      * @return Opportunity
      */
-    static function getBaseOpportunity(Opportunity $opportunity = null){
+    static function getBaseOpportunity(?Opportunity $opportunity = null){
         if(is_null($opportunity)){
             $opportunity = self::getRequestedOpportunity();
         }
